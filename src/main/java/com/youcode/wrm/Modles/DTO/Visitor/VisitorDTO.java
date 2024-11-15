5977132d82f782c4aa7087cbba1c376ed811deac
Aya <ayaelrhayour123@gmail.com>
2024-11-15 11:34:16 +0100

    created the visitor Dto

---
+++ src/main/java/com/youcode/wrm/Modles/DTO/Visitor/VisitorDTO.java
@@ -0,0 +1,23 @@
+package com.youcode.wrm.Modles.DTO.Visitor;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VisitorDTO {
+
+    private UUID id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+}
