18e1760a4922c7a8476a0a8c42da7c62252dc2d6
Aya <ayaelrhayour123@gmail.com>
2024-11-17 19:50:01 +0100

    created visiting list controller

---
+++ src/main/java/com/youcode/wrm/Controller/VisitingListController.java
@@ -0,0 +1,10 @@
+package com.youcode.wrm.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/visitingList")
+public class VisitingListController {
+}
