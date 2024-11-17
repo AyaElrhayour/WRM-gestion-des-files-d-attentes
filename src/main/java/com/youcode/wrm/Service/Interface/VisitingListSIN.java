2750ff27a67d393ee1555bdcbdc4348cd10022af
Aya <ayaelrhayour123@gmail.com>
2024-11-17 19:49:06 +0100

    created visiting list service interface

---
+++ src/main/java/com/youcode/wrm/Service/Interface/VisitingListSIN.java
@@ -0,0 +1,10 @@
+package com.youcode.wrm.Service.Interface;
+
+import com.youcode.wrm.Models.DTO.VisitingList.VisitingListDTO;
+
+import java.util.List;
+
+public interface VisitingListSIN {
+
+    List<VisitingListDTO> getAllVisitingLists();
+}
